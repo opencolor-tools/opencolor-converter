7255cc7f41e8bcd9bb1539f0ec9cb2cf3aca4d0f
Michael Schieben <babe@rockitbaby.de>
2016-05-18 14:58:58 +0200

    fixing test, latest build (css converter only)

--- test/lib.js
+++ test/lib.js
@@ -3,22 +3,28 @@ import {expect} from 'chai'
 import * as converter from '../lib'
 
 describe('Converter API', () => {
-  it('should convert less', () => {
+  xit('should convert less', () => {
     expect(converter).to.have.ownProperty('less')
   })
-  it('should respond to flatten', () => {
+  xit('should convert aco', () => {
     expect(converter).to.have.ownProperty('aco')
   })
-  it('should respond to searchAndReplace', () => {
+  it('should convert css', () => {
     expect(converter).to.have.ownProperty('css')
   })
-  it('should respond to autoname', () => {
+  xit('should convert sass', () => {
     expect(converter).to.have.ownProperty('sass')
   })
-  it('all comverter should be configurable', () => {
+  it('all converter should be provide import and export', () => {
     Object.keys(converter).forEach((key) => {
       expect(converter[key]).to.respondTo('importer')
       expect(converter[key]).to.respondTo('exporter')
+    })
+  })
+  it('all importer and exporter should be configurable', () => {
+    Object.keys(converter).forEach((key) => {
+      expect(converter[key]['importer']).to.have.ownProperty('configure')
+      expect(converter[key]['exporter']).to.have.ownProperty('configure')
     })
   })
 })
