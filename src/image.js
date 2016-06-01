ef821f1f9a188962fa2bdbc817b715ee840a5457
Christoph Labacher <christoph.labacher@gmail.com>
2016-06-01 12:53:49 +0200

    Multiple image importers

--- src/image.js
+++ src/image.js
@@ -2,72 +2,139 @@ import {createImporter, createExporter} from './factory'
 import oco from 'opencolor'
 import Color from 'color'
 import Vibrant from 'node-vibrant'
+import getColors from 'get-image-colors'
+import fileType from 'file-type'
 
 const defaultImporterOptions = {
-
+  importer: 'basic'
 }
 
 export const importer = createImporter(defaultImporterOptions, (input, options) => {
-  return new Promise((resolve, reject) => {
-    Vibrant.from(input).getPalette(function (err, palette) {
-      if (err) {
-        Promise.reject(err)
-      }
-
-      let paletteJson = {}
-
-      for (var key in palette) {
-        if (palette[key] !== null) {
-          paletteJson[key] = palette[key].getHex()
+  if (options.importer === 'vibrant') {
+    return new Promise((resolve, reject) => {
+      Vibrant.from(input).getPalette(function (err, palette) {
+        if (err) {
+          Promise.reject(err)
         }
-      }
 
-      paletteJson = JSON.stringify(paletteJson)
-      var tree = null
+        let paletteJson = {}
 
-      try {
-        tree = JSON.parse(paletteJson)
-      } catch (e) {
-        Promise.reject(e)
-      }
-
-      var ocoPalette = new oco.Entry()
-
-      function walk (key, value, path, parent, level) {
-        path = path.slice(0)
-        if (Object.prototype.toString.call(value) === '[object Array]') {
-          path.push(key)
-          value.forEach((item, index) => {
-            walk(index, item, path, value, level + 1)
-          })
-        } else if (typeof value === 'object') {
-          path.push(key)
-          Object.keys(value).forEach((k) => {
-            walk(k, value[k], path, value, level + 1)
-          })
-        } else {
-          var colorValue = null
-          try {
-            colorValue = Color(value)
-          } catch (e) {}
-          if (colorValue) {
-            var name = key
-            if (options.readNameFromKey && typeof parent === 'object' && options.keyForName in parent) {
-              name = parent[options.keyForName]
-            }
-            const color = new oco.Entry(name, [oco.ColorValue.fromColorValue(colorValue.hexString())])
-            path.push(name)
-            ocoPalette.set(path.join('.'), color)
+        for (var key in palette) {
+          if (palette[key] !== null) {
+            paletteJson[key] = palette[key].getHex()
           }
         }
-      }
-      Object.keys(tree).forEach((k) => {
-        walk(k, tree[k], [], tree, 0)
-      })
 
-      resolve(ocoPalette)
+        paletteJson = JSON.stringify(paletteJson)
+
+        var tree = null
+
+        try {
+          tree = JSON.parse(paletteJson)
+        } catch (e) {
+          Promise.reject(e)
+        }
+
+        var ocoPalette = new oco.Entry()
+
+        function walk (key, value, path, parent, level) {
+          path = path.slice(0)
+          if (Object.prototype.toString.call(value) === '[object Array]') {
+            path.push(key)
+            value.forEach((item, index) => {
+              walk(index, item, path, value, level + 1)
+            })
+          } else if (typeof value === 'object') {
+            path.push(key)
+            Object.keys(value).forEach((k) => {
+              walk(k, value[k], path, value, level + 1)
+            })
+          } else {
+            var colorValue = null
+            try {
+              colorValue = Color(value)
+            } catch (e) {}
+            if (colorValue) {
+              var name = key
+              if (options.readNameFromKey && typeof parent === 'object' && options.keyForName in parent) {
+                name = parent[options.keyForName]
+              }
+              const color = new oco.Entry(name, [oco.ColorValue.fromColorValue(colorValue.hexString())])
+              path.push(name)
+              ocoPalette.set(path.join('.'), color)
+            }
+          }
+        }
+        Object.keys(tree).forEach((k) => {
+          walk(k, tree[k], [], tree, 0)
+        })
+
+        resolve(ocoPalette)
+      })
     })
-  })
+  } else {
+    return new Promise((resolve, reject) => {
+      let mimeType = fileType(input).mime
+
+      getColors(input, mimeType, function (err, palette) {
+        if (err) {
+          Promise.reject(err)
+        }
+
+        let paletteJson = {}
+
+        for (var i = 0; i < palette.length; i++) {
+          paletteJson['Color' + i] = palette[i].hex()
+        }
+
+        paletteJson = JSON.stringify(paletteJson)
+
+        var tree = null
+
+        try {
+          tree = JSON.parse(paletteJson)
+        } catch (e) {
+          Promise.reject(e)
+        }
+
+        var ocoPalette = new oco.Entry()
+
+        function walk (key, value, path, parent, level) {
+          path = path.slice(0)
+          if (Object.prototype.toString.call(value) === '[object Array]') {
+            path.push(key)
+            value.forEach((item, index) => {
+              walk(index, item, path, value, level + 1)
+            })
+          } else if (typeof value === 'object') {
+            path.push(key)
+            Object.keys(value).forEach((k) => {
+              walk(k, value[k], path, value, level + 1)
+            })
+          } else {
+            var colorValue = null
+            try {
+              colorValue = Color(value)
+            } catch (e) {}
+            if (colorValue) {
+              var name = key
+              if (options.readNameFromKey && typeof parent === 'object' && options.keyForName in parent) {
+                name = parent[options.keyForName]
+              }
+              const color = new oco.Entry(name, [oco.ColorValue.fromColorValue(colorValue.hexString())])
+              path.push(name)
+              ocoPalette.set(path.join('.'), color)
+            }
+          }
+        }
+        Object.keys(tree).forEach((k) => {
+          walk(k, tree[k], [], tree, 0)
+        })
+
+        resolve(ocoPalette)
+      })
+    })
+  }
 })
 
 export const exporter = createExporter({}, (tree, options) => {
--- test/image.js
+++ test/image.js
@@ -10,15 +10,29 @@ describe('Image Converter', () => {
     it('should import png', () => {
       return importer(png).then((tree) => {
         expect(tree.children.length).to.be.greaterThan(1)
+        expect(tree.get('Color0')).to.not.be.undefined
+      })
+    })
+    const jpg = fs.readFileSync(path.join(__dirname, 'fixtures', 'image.jpg'))
+    it('should import jpg', () => {
+      return importer(jpg).then((tree) => {
+        expect(tree.children.length).to.be.greaterThan(1)
+        expect(tree.get('Color0')).to.not.be.undefined
+      })
+    })
+    it('should import png using Vibrant', () => {
+      return importer(png, {importer: 'vibrant'}).then((tree) => {
+        expect(tree.children.length).to.be.greaterThan(1)
         expect(tree.get('Vibrant')).to.not.be.undefined
         expect(tree.get('Muted')).to.not.be.undefined
         expect(tree.get('Vibrant').hexcolor()).to.equal('#232E90')
       })
     })
     const png2 = fs.readFileSync(path.join(__dirname, 'fixtures', 'image2.png'))
-    it('should import png, even when there are not all swatches', () => {
-      return importer(png2).then((tree) => {
+    it('should import png using Vibrant, even when there are not all swatches', () => {
+      return importer(png2, {importer: 'vibrant'}).then((tree) => {
         expect(tree.children.length).to.be.greaterThan(1)
+        expect(tree.get('Vibrant')).to.not.be.undefined
         expect(tree.get('DarkMuted')).to.be.undefined
       })
     })
