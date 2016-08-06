ee5c02ef30d08feb3f1335a43ca3ed282fcdec98
Christoph Labacher <christoph.labacher@gmail.com>
2016-08-06 16:55:53 +0200

    Test to Prove #235
    https://github.com/opencolor-tools/companion-app/issues/235

--- test/aco.js
+++ test/aco.js
@@ -5,13 +5,21 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 describe('ACO Converter', () => {
-  describe('Importer', () => {
+  describe.only('Importer', () => {
     const aco = fs.readFileSync(path.join(__dirname, 'fixtures', 'Material Palette.aco'))
     it('should import one variable', () => {
       return importer(aco).then((tree) => {
         expect(tree.children.length).to.be.greaterThan(1)
         expect(tree.get('Red 100')).to.not.be.undefined
         expect(tree.get('Blue 100')).to.not.be.undefined
+      })
+    })
+    const aco2 = fs.readFileSync(path.join(__dirname, 'fixtures', 'namics15_colors.aco'))
+    it('should import the right colors', () => {
+      return importer(aco2).then((tree) => {
+        console.log(tree)
+        expect(tree.children.length).to.be.greaterThan(1)
+        expect(tree.get('DigitalCommunications_yellow').hexcolor()).to.equal('#fbe937')
       })
     })
   })
